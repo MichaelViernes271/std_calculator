892b09df9bb24b78f6f55443ca72cb3324ee001d
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2023-11-05 16:18:28 +0800

    added the proper documentation comment blocks

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+/**
+ * Calculator App
+ *
+ * This JavaScript file defines a calculator class and handles user interactions
+ * to perform basic arithmetic calculations, convert numbers to binary, and manage memory.
+ *
+ * Author: Viernes, Michael
+ * Date: 05/11/2023
+ */
+
+/**
+ * Calculator Class
+ *
+ * This class represents a simple calculator with basic arithmetic operations,
+ * decimal-to-binary conversion, and memory functionality.
+ */
 class Calculator {
+   /**
+   * Constructor for the Calculator class.
+   *
+   * @param {HTMLElement} previousOperandTextElement - The element displaying the previous operand.
+   * @param {HTMLElement} currentOperandTextElement - The element displaying the current operand.
+   */
   constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.bin_value = false;
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
+    this.previousOperandTextElement = previousOperandTextElement // Previous numerical value on the display
+    this.currentOperandTextElement = currentOperandTextElement // Current number in the display
     this.memory = 0; // Initialize memory to 0
     this.clear()
   }
 
+  /**
+   * Clear the calculator's state, including current and previous operands, operation, and memory.
+   */
   clear() {
     this.currentOperand = ''
     this.previousOperand = ''
@@ -14,15 +38,28 @@ class Calculator {
     this.memory = 0
   }
 
+  /**
+   * Delete the last character from the current operand.
+   */
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
 
+  /**
+   * Append a number or decimal point to the current operand.
+   *
+   * @param {string} number - The number or decimal point to append.
+   */
   appendNumber(number) {
     if (number === '.' && this.currentOperand.includes('.')) return
     this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
+  /**
+   * Set the chosen operation for the calculator.
+   *
+   * @param {string} operation - The operation symbol (+, -, *, ÷).
+   */
   chooseOperation(operation) {
     if (this.currentOperand === '') return
     if (this.previousOperand !== '') {
@@ -33,6 +70,9 @@ class Calculator {
     this.currentOperand = ''
   }
 
+   /**
+   * Perform the calculation based on the chosen operation.
+   */
   compute() {
     let computation
     const prev = parseFloat(this.previousOperand)
@@ -59,6 +99,9 @@ class Calculator {
     this.previousOperand = ''
   }
 
+   /**
+   * Convert the current operand from decimal to binary.
+   */
   convertToBinary() {
     const currentValue = parseFloat(this.currentOperand);
     if (!isNaN(currentValue)) {
@@ -66,6 +109,9 @@ class Calculator {
     }
   }
 
+  /**
+   * Add the current operand to the calculator's memory.
+   */
   addToMemory() {
     const currentValue = parseFloat(this.currentOperand);
     if (!isNaN(currentValue)) {
@@ -74,6 +120,9 @@ class Calculator {
     }
   }
 
+  /**
+   * Subtract the current operand from the calculator's memory.
+   */
   subtractFromMemory() {
     const currentValue = parseFloat(this.currentOperand);
     if (!isNaN(currentValue)) {
@@ -82,6 +131,14 @@ class Calculator {
     }
   }
 
+  /**
+ * Get Display Number
+ *
+ * Formats a number for display in the calculator's output, ensuring proper thousands separators.
+ *
+ * @param {number} number - The number to format for display.
+ * @returns {string} - The formatted number for display.
+ */
   getDisplayNumber(number) {
     const stringNumber = number.toString()
     const integerDigits = parseFloat(stringNumber.split('.')[0])
@@ -90,6 +147,7 @@ class Calculator {
     if (isNaN(integerDigits)) {
       integerDisplay = ''
     } else {
+      // Format integer part with thousands separators for readability.
       integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
     }
     if (decimalDigits != null) {
@@ -99,6 +157,9 @@ class Calculator {
     }
   }
 
+  /**
+   * Update the display elements to show the current and previous operands.
+   */
   updateDisplay() {
     this.currentOperandTextElement.innerText =
       this.getDisplayNumber(this.currentOperand)
@@ -112,6 +173,7 @@ class Calculator {
 }
 
 
+// Query various button elements in the HTML for user interactions.
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -123,56 +185,65 @@ const binConvertButton = document.querySelector('[data-convert-binary]')
 const mPlusButton = document.querySelector('[data-m-plus]')
 const mMinusButton = document.querySelector('[data-m-minus]')
 
+// Instantiate a calculator Object.
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+// Event listeners for number buttons.
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
+    // Handle a click on a number button by appending the number to the current operand.
     calculator.appendNumber(button.innerText)
     calculator.updateDisplay()
   })
 })
 
+// Event listeners for operation buttons.
 operationButtons.forEach(button => {
   button.addEventListener('click', () => {
+    // Handle a click on an operation button by setting the chosen operation.
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
 })
 
 equalsButton.addEventListener('click', button => {
+  // Handle a click on the equals button by performing the calculation and updating the display.
   calculator.compute()
   calculator.updateDisplay()
 })
 
 allClearButton.addEventListener('click', button => {
+  // Handle a click on the all-clear button by resetting the calculator's state and updating the display.
   calculator.clear()
   calculator.updateDisplay()
 })
 
 deleteButton.addEventListener('click', button => {
+  // Handle a click on the delete button by removing the last character from the current operand and updating the display.
   calculator.delete()
   calculator.updateDisplay()
 })
 
 deleteButton.addEventListener('click', button => {
+   // Handle a click on the binary conversion button by converting the current operand to binary and updating the display.
   calculator.delete()
   calculator.updateDisplay()
 })
 
 binConvertButton.addEventListener('click', button => {
   // Handle binary conversion here
-  calculator.convertToBinary(); // Implement this method in your Calculator class
+  calculator.convertToBinary(); 
   calculator.updateDisplay();
 });
 
 mPlusButton.addEventListener('click', button => {
   // Handle Memory Add (M+) here
-  calculator.addToMemory(); // Implement this method in your Calculator class
+  calculator.addToMemory(); 
   calculator.updateDisplay();
 });
 
 mMinusButton.addEventListener('click', button => {
   // Handle Memory Subtract (M-) here
-  calculator.subtractFromMemory(); // Implement this method in your Calculator class
+  calculator.subtractFromMemory(); 
   calculator.updateDisplay();
 });
